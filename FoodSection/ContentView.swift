638299ee1acdc5b4b636b94e8b215ab82fec3afa
rudyalayon <rudyalayon@gmail.com>
2024-04-13 16:02:51 -0400

    Initial Commit

---
+++ FoodSection/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FoodSection
+//
+//  Created by Rudy Alayon on 2024-04-13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FoodSection/FoodSectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodSectionApp.swift
+//  FoodSection
+//
+//  Created by Rudy Alayon on 2024-04-13.
+//
+
+import SwiftUI
+
+@main
+struct FoodSectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
